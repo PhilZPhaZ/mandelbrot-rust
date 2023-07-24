ebe1b7b8bed5966cf192c6555b2501cdd68e36e9
PhilZPhaZ <bour.phileas@gmail.com>
2023-07-25 00:12:51 +0200

    started to add smoothing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 mod mandelbrot;
 
-use mandelbrot::mandelbrot;
-use minifb::{Window, WindowOptions, Key, KeyRepeat};
 use image::Rgb;
+use mandelbrot::mandelbrot;
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
 
-const WIDTH: usize = 1200;
-const HEIGHT: usize = 800;
+const WIDTH: usize = 600;
+const HEIGHT: usize = 400;
 
-fn zoom(x: f32, y: f32, x_min: &mut f64, x_max: &mut f64, y_min: &mut f64, y_max: &mut f64, zoom_factor: f64) {
+fn zoom(
+    x: f32,
+    y: f32,
+    x_min: &mut f64,
+    x_max: &mut f64,
+    y_min: &mut f64,
+    y_max: &mut f64,
+    zoom_factor: f64,
+) {
     let width: f64 = *x_max - *x_min;
     let height: f64 = *y_max - *y_min;
     let x_percent: f64 = x as f64 / WIDTH as f64;
@@ -25,58 +33,69 @@ fn main() {
     let mut y_min: f64 = -1.0;
     let mut y_max: f64 = 1.0;
 
-    let image: image::ImageBuffer<Rgb<u8>, Vec<u8>> = mandelbrot(x_min, x_max, y_min, y_max, WIDTH as u32, HEIGHT as u32);
+    let image: image::ImageBuffer<Rgb<u8>, Vec<u8>> =
+        mandelbrot(x_min, x_max, y_min, y_max, WIDTH as u32, HEIGHT as u32);
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
             let pixel_value: &Rgb<u8> = image.get_pixel(x as u32, y as u32);
-            let color: u32 = ((pixel_value[0] as u32) << 16) | ((pixel_value[1] as u32) << 8) | pixel_value[2] as u32;
+            let color: u32 = ((pixel_value[0] as u32) << 16)
+                | ((pixel_value[1] as u32) << 8)
+                | pixel_value[2] as u32;
 
             buffer[y * WIDTH + x] = color;
         }
     }
 
     let mut window = Window::new(
-        "Test",
-        WIDTH, 
-        HEIGHT, 
-        WindowOptions::default()
+        "Mandelbrot explorer",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
     )
-    .unwrap_or_else(|e|{
+    .unwrap_or_else(|e| {
         panic!("{}", e);
     });
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         if window.is_key_pressed(Key::Space, KeyRepeat::No) {
-            if let Some((x, y)) = window.get_mouse_pos(minifb::MouseMode::Discard) { 
+            if let Some((x, y)) = window.get_mouse_pos(minifb::MouseMode::Discard) {
                 zoom(x, y, &mut x_min, &mut x_max, &mut y_min, &mut y_max, 0.5);
                 buffer = update_buffer(x_min, x_max, y_min, y_max, WIDTH, HEIGHT)
             }
         };
         if window.is_key_pressed(Key::Backspace, KeyRepeat::No) {
-            if let Some((x, y)) = window.get_mouse_pos(minifb::MouseMode::Discard) { 
+            if let Some((x, y)) = window.get_mouse_pos(minifb::MouseMode::Discard) {
                 zoom(x, y, &mut x_min, &mut x_max, &mut y_min, &mut y_max, -0.5);
                 buffer = update_buffer(x_min, x_max, y_min, y_max, WIDTH, HEIGHT)
             }
         };
 
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
 
-fn update_buffer(x_min: f64, x_max: f64, y_min: f64, y_max: f64, width: usize, height: usize) -> Vec<u32> {
-    let image: image::ImageBuffer<Rgb<u8>, Vec<u8>> = mandelbrot(x_min, x_max, y_min, y_max, width as u32, height as u32);
+fn update_buffer(
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+    width: usize,
+    height: usize,
+) -> Vec<u32> {
+    let image: image::ImageBuffer<Rgb<u8>, Vec<u8>> =
+        mandelbrot(x_min, x_max, y_min, y_max, width as u32, height as u32);
 
     let mut buffer: Vec<u32> = vec![0; width * height];
 
     for y in 0..height {
         for x in 0..width {
             let pixel_value: &Rgb<u8> = image.get_pixel(x as u32, y as u32);
-            let color: u32 = ((pixel_value[0] as u32) << 16) | ((pixel_value[1] as u32) << 8) | pixel_value[2] as u32;
+            let color: u32 = ((pixel_value[0] as u32) << 16)
+                | ((pixel_value[1] as u32) << 8)
+                | pixel_value[2] as u32;
 
             buffer[y * width + x] = color;
         }
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,24 +1,25 @@
-use image::{Rgb, ImageBuffer, RgbImage};
+use image::{ImageBuffer, Rgb, RgbImage};
 
-const MAX_ITER: usize = 200;
+const MAX_ITER: usize = 1000;
 
+#[warn(dead_code)]
 const COLOR_PALETTE: [(u8, u8, u8); 16] = [
-   ( 66,  30,  15),
-   ( 25,   7,  26),
-   (  9,   1,  47),
-   (  4,   4,  73),
-   (  0,   7, 100),
-   ( 12,  44, 138),
-   ( 24,  82, 177),
-   ( 57, 125, 209),
-   (134, 181, 229),
-   (211, 236, 248),
-   (241, 233, 191),
-   (248, 201,  95),
-   (255, 170,   0),
-   (204, 128,   0),
-   (153,  87,   0),
-   (106,  52,   3)
+    (66, 30, 15),
+    (25, 7, 26),
+    (9, 1, 47),
+    (4, 4, 73),
+    (0, 7, 100),
+    (12, 44, 138),
+    (24, 82, 177),
+    (57, 125, 209),
+    (134, 181, 229),
+    (211, 236, 248),
+    (241, 233, 191),
+    (248, 201, 95),
+    (255, 170, 0),
+    (204, 128, 0),
+    (153, 87, 0),
+    (106, 52, 3),
 ];
 
 fn is_mandel(cx: f64, cy: f64) -> Rgb<u8> {
@@ -27,27 +28,49 @@ fn is_mandel(cx: f64, cy: f64) -> Rgb<u8> {
     let mut iter: usize = 0;
 
     while x * x + y * y <= 4.0 && iter < MAX_ITER {
-        let xtemp: f64 = x*x - y*y + cx;
-        y = 2.0*x*y + cy;
+        let xtemp: f64 = x * x - y * y + cx;
+        y = 2.0 * x * y + cy;
         x = xtemp;
 
         iter += 1;
     }
 
-    // let color_value: u8 = 255 - (iter as f64 * 255.0 / MAX_ITER as f64) as u8;
-    // Rgb([color_value, color_value, color_value])
     if iter == MAX_ITER {
         Rgb([0, 0, 0])
     } else {
-        let color_i: u8 = (iter % 16) as u8;
+        let log_zn: f64 = (x * x + y * y).ln();
+        let nu: f64 = (log_zn / 2.0_f64.ln()).ln() / (2.0_f64.ln());
 
-        let color: (u8, u8, u8) = COLOR_PALETTE[color_i as usize];
-        
-        Rgb([color.0, color.1, color.2])
+        // Effectuer l'interpolation pour obtenir des valeurs réelles pour les coordonnées non entières.
+        let smooth_iter: f64 = (iter as f64) + 1.0 - nu;
+
+        // Obtenir les parties entières et fractionnaires de la valeur lissée.
+        let smooth_iter_floor: usize = smooth_iter.floor() as usize;
+        let smooth_iter_frac: f64 = smooth_iter - (smooth_iter_floor as f64);
+
+        // Interpolation linéaire entre les couleurs de la palette.
+        let color1: (u8, u8, u8) = COLOR_PALETTE[smooth_iter_floor % COLOR_PALETTE.len()];
+        let color2: (u8, u8, u8) = COLOR_PALETTE[(smooth_iter_floor + 1) % COLOR_PALETTE.len()];
+
+        let r: u8 = ((color1.0 as f64) * (1.0 - smooth_iter_frac)
+            + (color2.0 as f64) * smooth_iter_frac) as u8;
+        let g: u8 = ((color1.1 as f64) * (1.0 - smooth_iter_frac)
+            + (color2.1 as f64) * smooth_iter_frac) as u8;
+        let b: u8 = ((color1.2 as f64) * (1.0 - smooth_iter_frac)
+            + (color2.2 as f64) * smooth_iter_frac) as u8;
+
+        Rgb([r, g, b])
     }
 }
 
-pub fn mandelbrot(re_start: f64, re_end: f64, im_start: f64, im_end: f64, width: u32, height: u32) -> RgbImage {
+pub fn mandelbrot(
+    re_start: f64,
+    re_end: f64,
+    im_start: f64,
+    im_end: f64,
+    width: u32,
+    height: u32,
+) -> RgbImage {
     let mut image: RgbImage = ImageBuffer::new(width, height);
 
     for (x, y, pixel) in image.enumerate_pixels_mut() {
@@ -55,7 +78,6 @@ pub fn mandelbrot(re_start: f64, re_end: f64, im_start: f64, im_end: f64, width:
         let cy: f64 = im_start + (y as f64 / height as f64) * (im_end - im_start);
 
         *pixel = is_mandel(cx, cy);
-    };
-    println!("Done");
+    }
     image
 }
